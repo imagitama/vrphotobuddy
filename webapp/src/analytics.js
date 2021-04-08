78ceff41db9e1fe1c1be2b51688b891c1f47dd5b
Jared Williams <accounts@jaredwilliams.com.au>
2021-04-08 17:00:09 +0800

    Fix error

--- webapp/src/analytics.js
+++ webapp/src/analytics.js
@@ -6,6 +6,10 @@ export const trackAction = (category, action, payload) => {
     return
   }
 
+  if (!window.gtag) {
+    return
+  }
+
   window.gtag('event', action, {
     event_category: category,
     event_label: JSON.stringify(payload)
